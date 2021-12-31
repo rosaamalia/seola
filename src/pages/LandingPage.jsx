1a4d8a0c787fb77311795b6556436d380ce379dc
rosaamalia <amaliarosaaa15@gmail.com>
2021-12-31 08:32:32 +0700

    menambahkan navbar dan footer

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+function LandingPage(){
+    return (
+        <React.Fragment>
+            <Container className="min-vh-100">
+                <h1>Landing Page</h1>
+            </Container>
+        </React.Fragment>
+    )
+}
+
+export default LandingPage;
---
+++ src/pages/RegisterPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+function RegisterPage(){
+    return (
+        <React.Fragment>
+            <Container>
+                <h1>Register Page</h1>
+            </Container>
+        </React.Fragment>
+    )
+}
+
+export default RegisterPage;
