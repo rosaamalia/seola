13e7b1ae85128fa22679e981a501e28329a185ba
rosaamalia <amaliarosaaa15@gmail.com>
2022-01-06 11:33:52 +0700

    menambahkan modal gambar porotoflio

--- src/components/ProfileComponents/PortofolioList.jsx
+++ src/components/ProfileComponents/PortofolioList.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './style.css';
 import { Card, Row, Col } from 'react-bootstrap';
+import ModalImage from 'react-modal-image';
 
 function PortofolioList() {
     return ( 
@@ -9,10 +10,34 @@ function PortofolioList() {
             <Card style={{ width: '100%', height: 'auto', padding: '2rem' }}>
                 <h3 className="m-0 p-0 align-self-start">Portofolio</h3>
                 <div className="image-portofolio-container">
-                    <img className="preview-portofolio" src="https://i.pinimg.com/564x/95/4d/97/954d97097205bcc1bb872889819928eb.jpg"></img>
-                    <img className="preview-portofolio" src="https://i.pinimg.com/564x/ab/8a/34/ab8a346cf0051c5431ebcb8779158c1e.jpg"></img>
-                    <img className="preview-portofolio" src="https://i.pinimg.com/564x/42/86/58/428658803ed7ef232aea2eb22ab9c5f3.jpg"></img>
-                    <img className="preview-portofolio" src="https://i.pinimg.com/564x/80/90/28/809028c982c3e8be24597e55c93a13a6.jpg"></img>
+                    <div>
+                    <ModalImage
+                        small="https://i.pinimg.com/564x/95/4d/97/954d97097205bcc1bb872889819928eb.jpg"
+                        medium="https://i.pinimg.com/564x/95/4d/97/954d97097205bcc1bb872889819928eb.jpg"
+                        large="https://i.pinimg.com/564x/95/4d/97/954d97097205bcc1bb872889819928eb.jpg"
+                        className="preview-portofolio"/>
+                    </div>
+                    <div>
+                    <ModalImage
+                        small="https://i.pinimg.com/564x/ab/8a/34/ab8a346cf0051c5431ebcb8779158c1e.jpg"
+                        medium="https://i.pinimg.com/564x/ab/8a/34/ab8a346cf0051c5431ebcb8779158c1e.jpg"
+                        large="https://i.pinimg.com/564x/ab/8a/34/ab8a346cf0051c5431ebcb8779158c1e.jpg"
+                        className="preview-portofolio"/>
+                    </div>
+                    <div>
+                    <ModalImage
+                        small="https://i.pinimg.com/564x/42/86/58/428658803ed7ef232aea2eb22ab9c5f3.jpg"
+                        medium="https://i.pinimg.com/564x/42/86/58/428658803ed7ef232aea2eb22ab9c5f3.jpg"
+                        large="https://i.pinimg.com/564x/42/86/58/428658803ed7ef232aea2eb22ab9c5f3.jpg"
+                        className="preview-portofolio"/>
+                    </div>
+                    <div>
+                    <ModalImage
+                        small="https://i.pinimg.com/564x/80/90/28/809028c982c3e8be24597e55c93a13a6.jpg"
+                        medium="https://i.pinimg.com/564x/80/90/28/809028c982c3e8be24597e55c93a13a6.jpg"
+                        large="https://i.pinimg.com/564x/80/90/28/809028c982c3e8be24597e55c93a13a6.jpg"
+                        className="preview-portofolio"/>
+                    </div>
                 </div>
                 <div className="portofolio-button">
                     <Link to="/portofolio" className="portofolio-link">LIHAT SEMUA PORTOFOLIO</Link>
