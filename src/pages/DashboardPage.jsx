28daa0c5e519b73ada0d9d0b1ec7aae8ead4b667
rosaamalia <amaliarosaaa15@gmail.com>
2022-01-06 21:34:55 +0700

    menambahkan rincian kelas

---
+++ src/pages/DashboardPage.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Container, Card, Col, Row } from "react-bootstrap";
+
+import Kelas from '../components/DashboardComponents/Kelas';
+import PreviewProfil from '../components/DashboardComponents/PreviewProfil';
+import Leaderboard from '../components/DashboardComponents/Leaderboard';
+
+function DashboardPage(){
+    return (
+        <Container fluid className="background-page min-vh-100 px-5 d-flex justify-content-center align-items-start">
+            <Col lg={7} className="align-self-start mt-5">
+                <Kelas></Kelas>
+            </Col>
+            <Col lg={4} className="mt-5">
+                <Card></Card>
+            </Col>
+        </Container>
+    )
+}
+
+export default DashboardPage;
