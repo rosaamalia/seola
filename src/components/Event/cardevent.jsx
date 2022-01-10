aef6800786372b23ab704a54c2cd79c1fa59ac39
Ghinafairuznazilah <ghina.fairuz.nazilah@gmail.com>
2022-01-10 12:10:30 +0700

    update button & responsive

--- src/components/Event/cardevent.jsx
+++ src/components/Event/cardevent.jsx
@@ -22,10 +22,10 @@ function CardEvent (){
             
             </Row> */}
 
-            <Row xs={1} md={3} className="mx-2 my-2" >
+            <Row xs={1} md={3} sm={1} lg={3} className="mx-2 my-2" >
             {Array.from({ length: 10 }).map((_, idx) => (
                 <Col>
-                <Card style={{ width: '18rem', height: '30rem', maxWidth: '18rem', maxHeight: '30rem', borderRadius : 11 }}>
+                <Card style={{ width: '18rem', height: '30rem', maxWidth: '18rem', maxHeight: '30rem', borderRadius : 11 }} className="mx-auto my-auto mb-5">
                     <Card.Img variant="top" src="https://images.unsplash.com/photo-1499781350541-7783f6c6a0c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=945&q=80"
                     />
                     <Card.Body>
@@ -35,12 +35,12 @@ function CardEvent (){
                     <Card.Text style={{color: "black", textAlign:"justify", fontWeight:"normal" }} className="mb-4">
                     Hasil karya luar biasa dari anak-anak seni musik untuk menampilkan karya mereka, saksikan pentasnya. yuk buruan daftar!
                     </Card.Text>
-                    <Button variant="primary" type="submit" style={{backgroundColor: "#242A55", color: "white", minWidth:100}} className="mb-2" >Ikuti</Button>
+                    <Button variant="primary" type="submit" style={{backgroundColor: "#242A55", color: "white", minWidth:100}} className="mb-2 d-block mx-lg-auto mx-md-auto mx-sm-auto" >Ikuti</Button>
                     </Card.Body>
                 </Card>
                 </Col>
             ))}
-</Row>
+            </Row>
         </Container>
     )
 }
