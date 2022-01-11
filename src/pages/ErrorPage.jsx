79e071089d98c3446f2a41d5739bff46e7cf600c
Singgih Pratama <78544446+mastama@users.noreply.github.com>
2022-01-11 14:37:19 +0700

    Membuat Error Page

---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Error from "../components/ErrorScreen/Error";
+// import { Container } from "react-bootstrap";
+
+const ErrorPage = () => {
+  return <Error></Error>;
+};
+
+export default ErrorPage;
